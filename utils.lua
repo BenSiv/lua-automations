2f01efbb8c78b271b27f2671237d288cb920f298
BenSiv <bensiv92@gmail.com>
2023-09-28 14:11:20 +0300

    initilizing arch automations with lua

---
+++ utils.lua
@@ -0,0 +1,80 @@
+-- Define a module table
+local utils = {}
+
+function utils.length(table)
+    local len = #table
+    return len
+end
+
+function utils.get_script_dir()
+    local script_path = arg[0]
+    local script_dir = script_path:match("(.-)[^/]+$")
+    return script_dir
+end
+
+
+local function copy_table(table)
+    local copy = {}
+    for key, value in pairs(table) do
+        if type(value) == "table" then
+            copy[key] = copy_table(value)
+        else
+            copy[key] = value
+        end
+    end
+    return copy
+end
+
+function utils.copy(source)
+    if type(source) == "table" then
+        copy = copy_table(source)
+    else
+        copy = source
+    end
+    return copy
+end
+
+function utils.empty(reference)
+    local new_var
+
+    if type(reference) == "number" then
+        new_var = 0 -- Initialize as a number
+    elseif type(reference) == "string" then
+        new_var = "" -- Initialize as a string
+    elseif type(reference) == "table" then
+        new_var = {} -- Initialize as a table
+    end
+
+    return new_var
+end
+
+local function slice_table(source, start_index, end_index)
+    local result = {}
+    for i = start_index, end_index do
+        if source[i] then
+            table.insert(result, source[i])
+        else
+            error("ERROR: index is out of range")
+            break
+        end
+    end
+    return result
+end
+
+local function slice_string(source, start_index, end_index)
+    return source:sub(start_index, end_index)
+end
+
+function utils.slice(source, start_index, end_index)
+    if type(source) == "table" then
+        result = slice_table(source, start_index, end_index)
+    elseif type(source) == "string" then
+        result = slice_string(source, start_index, end_index)
+    else
+        error("ERROR: can't slice element of type: " .. type(source))
+    end
+    return result
+end
+
+-- Export the module
+return utils
