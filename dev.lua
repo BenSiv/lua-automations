bf43578058080dcc552259a4e7abfec07c2e0113
bensiv <bensiv92@gmail.com>
2025-06-19 16:02:19 +0300

    changing from docker to podman

--- dev.lua
+++ dev.lua
@@ -2,10 +2,10 @@
 
 local function main()
 	local name = arg[1] or "celleste-dev"
-	local start_container = string.format("docker start '%s'", name)
+	local start_container = string.format("podman start '%s'", name)
 	os.execute(start_container)
 	
-	local open_container = string.format("docker exec -it '%s' bash", name)
+	local open_container = string.format("podman exec -it '%s' bash", name)
 	os.execute(open_container)
 end
 
